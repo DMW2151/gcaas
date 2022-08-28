4f43077d0c330560352b7188cd2fc0851a22e7d3
Dustin Wilson <dmw2151@columbia.edu>
2022-08-28 13:09:35 -0400

    update to docs

--- infra/do-container-reg.tf
+++ infra/do-container-reg.tf
@@ -14,40 +14,80 @@ resource "digitalocean_container_registry_docker_credentials" "gcaas" {
   registry_name = digitalocean_container_registry.gcaas.name
 }
 
-// build grpc service image locally
-// resource: https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/image#build
-resource "docker_image" "gcaas_grpc" {
-  name = "${digitalocean_container_registry.gcaas.endpoint}/gcaas-grpc:0.0.1"
-  build {
-    path       = "${path.module}/.."
-    dockerfile = "${path.module}/../srv/cmd/grpc/Dockerfile"
-  }
-  triggers = {
-    src_code_hash = sha1(join("", [for f in fileset(path.module, "./../srv/**") : filesha1(f)]))
-  }
-}
-
 // build http service image locally
 // resource: https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/image#build
 resource "docker_image" "gcaas_edge" {
   name = "${digitalocean_container_registry.gcaas.endpoint}/gcaas-edge:0.0.1"
   build {
     path       = "${path.module}/.."
-    dockerfile = "${path.module}/../srv/cmd/public/Dockerfile"
+    dockerfile = "${path.module}/../geocoder-svc/cmd/edge/Dockerfile"
   }
   triggers = {
-    src_code_hash = sha1(join("", [for f in fileset(path.module, "./../srv/**") : filesha1(f)]))
+    src_code_hash = sha1(join("", [for f in fileset(path.module, "./../geocoder-svc/**") : filesha1(f)]))
   }
 }
 
-// push grpc service to DOCR
-// resource: https://registry.terraform.io/providers/hashicorp/null/latest/docs/resources/resource
-resource "null_resource" "docr_push_gcaas_grpc" {
-  provisioner "local-exec" {
-    command = "doctl registry login && docker push ${digitalocean_container_registry.gcaas.endpoint}/gcaas-grpc:0.0.1"
+// build grpc service image locally
+// resource: https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/image#build
+resource "docker_image" "gcaas_geocoder" {
+  name = "${digitalocean_container_registry.gcaas.endpoint}/gcaas-geocoder:0.0.1"
+  build {
+    path       = "${path.module}/.."
+    dockerfile = "${path.module}/../geocoder-svc/cmd/geocoder/Dockerfile"
   }
   triggers = {
-    image_id_w_sha = docker_image.gcaas_grpc.id
+    src_code_hash = sha1(join("", [for f in fileset(path.module, "./../geocoder-svc/**") : filesha1(f)]))
+  }
+}
+
+// build batch service image locally
+// resource: https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/image#build
+resource "docker_image" "gcaas_batch" {
+  name = "${digitalocean_container_registry.gcaas.endpoint}/gcaas-batch:0.0.1"
+  build {
+    path       = "${path.module}/.."
+    dockerfile = "${path.module}/../geocoder-svc/cmd/batch/Dockerfile"
+  }
+  triggers = {
+    src_code_hash = sha1(join("", [for f in fileset(path.module, "./../geocoder-svc/**") : filesha1(f)]))
+  }
+}
+
+// build batch service image locally
+// resource: https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/image#build
+resource "docker_image" "gcaas_worker" {
+  name = "${digitalocean_container_registry.gcaas.endpoint}/gcaas-worker:0.0.1"
+  build {
+    path       = "${path.module}/.."
+    dockerfile = "${path.module}/../geocoder-svc/cmd/worker/Dockerfile"
+  }
+  triggers = {
+    src_code_hash = sha1(join("", [for f in fileset(path.module, "./../geocoder-svc/**") : filesha1(f)]))
+  }
+}
+
+// build batch service image locally
+// resource: https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/image#build
+resource "docker_image" "gcaas_mgmt" {
+  name = "${digitalocean_container_registry.gcaas.endpoint}/gcaas-mgmt:0.0.1"
+  build {
+    path       = "${path.module}/.."
+    dockerfile = "${path.module}/../geocoder-svc/cmd/mgmt/Dockerfile"
+  }
+  triggers = {
+    src_code_hash = sha1(join("", [for f in fileset(path.module, "./../geocoder-svc/**") : filesha1(f)]))
+  }
+}
+
+
+// push grpc service to DOCR
+// resource: https://registry.terraform.io/providers/hashicorp/null/latest/docs/resources/resource
+resource "null_resource" "docr_push_gcaas_geocoder" {
+  provisioner "local-exec" {
+    command = "docker push ${digitalocean_container_registry.gcaas.endpoint}/gcaas-geocoder:0.0.1"
+  }
+  triggers = {
+    image_id_w_sha = docker_image.gcaas_geocoder.id
   }
 }
 
@@ -55,7 +95,7 @@ resource "null_resource" "docr_push_gcaas_grpc" {
 // resource: https://registry.terraform.io/providers/hashicorp/null/latest/docs/resources/resource
 resource "null_resource" "docr_push_gcaas_edge" {
   provisioner "local-exec" {
-    command = "doctl registry login && docker push ${digitalocean_container_registry.gcaas.endpoint}/gcaas-edge:0.0.1"
+    command = "docker push ${digitalocean_container_registry.gcaas.endpoint}/gcaas-edge:0.0.1"
   }
   triggers = {
     image_id_w_sha = docker_image.gcaas_edge.id
